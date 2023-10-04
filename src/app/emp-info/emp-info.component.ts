030ba9500d97de3ea5992abe087ea079cadbc2c2
Mehrshad <lastenigmas@gmail.com>
2023-10-04 11:18:00 +0330

    now i can work with angular

--- src/app/emp-info/emp-info.component.ts
+++ src/app/emp-info/emp-info.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { RecordsService } from '../records.service';
 @Component({
   selector: 'app-emp-info',
   templateUrl: './emp-info.component.html',
-  styleUrls: ['./emp-info.component.css']
+  styleUrls: ['./emp-info.component.css'],
+  providers: [RecordsService]
 })
-export class EmpInfoComponent {
+export class EmpInfoComponent implements OnInit {
 
+
+  infoRec1: string[] = [];
+  infoRec2: string[] = [];
+  infoRec3: string[] = [];
+
+  getInfo1(){
+    this.infoRec1 = this.rservices.getInfo1()
+  }
+  
+  constructor(private rservices: RecordsService) { }
+
+  ngOnInit(): void {}
 }
--- src/app/records.service.ts
+++ src/app/records.service.ts
@@ -4,6 +4,20 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class RecordsService {
+  info1: string[] = ["Adam Tylor" , "2eE" , "asadi"]
+  info2: string[] = ["mehrshad" , "2esdE" , "milni"]
+  info3: string[] = ["arya" , "2x2eE" , "tehrany"]
+
+
+  getInfo1(): string[]{
+    return this.info1
+  }
+  getInfo2(): string[]{
+    return this.info2
+  }
+  getInfo3(): string[]{
+    return this.info3
+  }
 
   constructor() { }
 }
