904a984c1a6bb9430640816f4ea8bb37deab99ec
Mehrshad <lastenigmas@gmail.com>
2023-10-04 10:40:29 +0330

    hello-angular

---
+++ src/app/imagecomponent/imagecomponent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'n-image',
+  templateUrl: './imagecomponent.component.html',
+  styleUrls: ['./imagecomponent.component.css']
+})
+export class ImagecomponentComponent {
+
+}
